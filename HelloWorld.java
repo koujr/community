d2bb91d5892094f1673e8561f9d7358bc8edcf5e
koujr <1062590935@qq.com>
2019-07-28 17:01:52 +0800

    helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+﻿public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("1");
+		System.out.println("Hello World");
+		System.out.println("2");
+	}
+}
